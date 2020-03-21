b97281d9ad755b6db4671eb931b073303b60e8aa
raghava giri <giriraghava1970@gmail.com>
2020-03-21 19:06:37 +0530

    first commit

---
+++ app/Service/HomeService.js
@@ -0,0 +1,24 @@
+import HttpClient from '@Utils/HttpClient'
+
+
+function CalenderData(staffId) {
+    let endpoint = 'schedules/';
+    let data = {
+        "staffID" : staffId
+    }
+    return HttpClient.post(endpoint,data);
+}
+function keyList(key) {
+    let endpoint = 'key_list.php?key='+key;
+    return HttpClient.put(endpoint);
+}
+function Postdetails(id) {
+    let endpoint = 'postdetails.php?id='+id;
+    return HttpClient.put(endpoint);
+}
+
+export default {
+    CalenderData,
+    Postdetails,
+    keyList
+}
