d8659accab48f5914628263ab6cd701dd61cbdf5
raghava giri <giriraghava1970@gmail.com>
2020-03-24 12:15:01 +0530

    re check

---
+++ app/Service/MapService.js
@@ -0,0 +1,19 @@
+import HttpClient from '@Utils/HttpClient'
+import Storage from '@Utils/Storage'
+import { AsyncStorage } from 'react-native';
+
+
+function MapPing() {
+    let endpoint = 'mapPing.php';
+    return HttpClient.put(endpoint);
+}
+
+async function startShift(data) {
+    let endpoint = 'punchDetails';
+    return HttpClient.put(endpoint, data);
+}
+
+export default {
+    MapPing,
+    startShift
+}
